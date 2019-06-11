788ef9182fea068e90dfb3f2839577c552289499
jmisxiaofeixia <50257395+jmisxiaofeixia@users.noreply.github.com>
2019-06-11 11:48:44 +0800

    Revert "Revert "Revert "clear"""
    This reverts commit ed96784f6c14b25f424474728b0c3fe49b4d8e17.

---
+++ 1.py
@@ -0,0 +1,21 @@
+'''mood = False
+if mood :
+    print('go left')
+print('go back')
+
+
+account = 'qiyue'
+password = '123456'
+
+print('please input account')
+user_account = input()
+print('please input password')
+user_password = input()
+
+if account == user_account and password == user_password:
+    print('success')
+else:
+    print('fail')
+'''
+
+
